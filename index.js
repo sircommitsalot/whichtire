8fcd8f49d1ae75c9689bd05cc6362d5f33a27a9f
James Prow <jkprow@gmail.com>
2022-03-08 12:45:40 -0800

    shows tire change in response to rim change

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 const svg = document.getElementById('svg');
 const ctx = svg.getContext('2d');
 
-const width = svg.width / 2;
-const height = svg.height / 2;
-const radius = width - ctx.lineWidth;
-const startAngle = Math.PI;
-const endAngle = Math.PI * 2;
+const centroidX = svg.width / 2;
+const centroidY = svg.height / 2;
+const startRadius = (centroidX - ctx.lineWidth) / 2;
+const startAngle = Math.PI * 0.5;
+const endAngle = Math.PI * 2.5;
 
 const renderCenterPoint = () => {
   ctx.beginPath();
-  ctx.arc(width, height, radius / 100, 0, Math.PI * 2);
+  ctx.arc(centroidX, centroidY, startRadius / 100, 0, Math.PI * 2);
   ctx.fill();
 }
 
-const renderTireAndRim = angleOffset => {
+const renderTireAndRim = (radius, angleOffset) => {
   ctx.beginPath();
   ctx.setLineDash([]);
-  ctx.arc(width, height, radius, startAngle - angleOffset, endAngle + angleOffset);
+  ctx.arc(centroidX, centroidY, radius, startAngle + angleOffset, endAngle - angleOffset);
   ctx.closePath();
   ctx.stroke();
 }
 
-const renderVirtualTireBottom = angleOffset => {
+const renderVirtualTireBottom = (radius, angleOffset) => {
   ctx.beginPath();
   ctx.setLineDash([5, 15]);
-  ctx.arc(width, height, radius, startAngle - angleOffset, endAngle + angleOffset, true);
+  ctx.arc(centroidX, centroidY, radius, startAngle + angleOffset, endAngle - angleOffset, true);
   ctx.stroke();
 }
 
 const render = (sliderPercentage = 0) => {
-  const angleOffset = sliderPercentage / 100 * Math.PI / 2;
+  const angleOffset = sliderPercentage * Math.PI / 2;
+  const chordLength = sliderPercentage * svg.width;
+  const radiusContribution = chordLength / 4;
+  const radius = startRadius + radiusContribution;
   
   ctx.clearRect(0, 0, svg.width, svg.height);
   renderCenterPoint();
-  renderTireAndRim(angleOffset);
-  renderVirtualTireBottom(angleOffset);
+  renderTireAndRim(radius, angleOffset);
+  renderVirtualTireBottom(radius, angleOffset);
 }
 
 const handleSliderInput = e => {
   const val = e.target.value;
-  render(val);
+  render(val / 100);
 }
 
 document.getElementById('range').addEventListener('input', handleSliderInput);
