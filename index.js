95ebce49fac1ec2ffc39e4039397985187ce0437
James Prow <jkprow@gmail.com>
2022-03-09 12:12:47 -0800

    better scaling

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const canvas = document.getElementById('canvas');
 
 const styles = getComputedStyle(canvas.parentElement);
-const w = parseInt(styles.getPropertyValue("width"), 10);
-const h = parseInt(styles.getPropertyValue("height"), 10);
+const w = parseInt(styles.getPropertyValue('width'), 10);
+const h = parseInt(styles.getPropertyValue('height'), 10);
 
-canvas.width = w;
-canvas.height = h;
+const smallDimension = Math.min(w, h);
+
+canvas.width = smallDimension;
+canvas.height = smallDimension;
 
 const ctx = canvas.getContext('2d');
 
 const centroidX = canvas.width / 2;
 const centroidY = canvas.height / 2;
-const startRadius = (centroidX - ctx.lineWidth) / 2;
+const canvasRadius = (centroidX - ctx.lineWidth) / 2;
 const startAngle = Math.PI * 0.5;
 const endAngle = Math.PI * 2.5;
 
-const tireFlatWidth = startRadius * 2 * Math.PI;
-
-const scaleFactor = 110.2 / tireFlatWidth;
-
 const tireFlatWidthEl = document.getElementById('tire-flat-width');
-tireFlatWidthEl.value = tireFlatWidth * scaleFactor;
+tireFlatWidthEl.value = 110;
 tireFlatWidthEl.disabled = true;
 
 const rimWidthEl = document.getElementById('rim-width');
@@ -29,11 +27,12 @@ const tireHeightEl = document.getElementById('tire-height');
 
 const renderCenterPoint = () => {
   ctx.beginPath();
-  ctx.arc(centroidX, centroidY, startRadius / 100, 0, Math.PI * 2);
+  ctx.arc(centroidX, centroidY, canvasRadius / 100, 0, Math.PI * 2);
   ctx.fill();
 }
 
 const renderTireAndRim = (radius, angleOffset) => {
+  console.log(radius)
   ctx.beginPath();
   ctx.setLineDash([]);
   ctx.arc(centroidX, centroidY, radius, startAngle + angleOffset, endAngle - angleOffset);
@@ -49,23 +48,26 @@ const renderVirtualTireBottom = (radius, angleOffset) => {
 }
 
 const render = (chord = 0) => {
-  const scaledChord = chord / scaleFactor;
-  const halfChord = scaledChord / 2;
+  const flatWidth = parseInt(tireFlatWidthEl.value, 10)
+  const startRadius = flatWidth / Math.PI / 2;
+  const scaleFactor = canvasRadius / startRadius;
+  
+  const halfChord = chord / 2;
   const radius = startRadius + halfChord / 2;
   
   const diameter = radius * 2;
   const sagitta = radius - Math.sqrt(radius * radius - halfChord * halfChord);
   const tireHeight = diameter - sagitta;
   
-  tireWidthEl.innerText = String((diameter * scaleFactor).toFixed(2));
-  tireHeightEl.innerText = String((tireHeight * scaleFactor).toFixed(2));
+  tireWidthEl.innerText = String(diameter.toFixed(2));
+  tireHeightEl.innerText = String(tireHeight.toFixed(2));
   
-  const angleOffset = scaledChord / canvas.width * Math.PI / 2;
+  const angleOffset = (chord * scaleFactor) / canvas.width * Math.PI / 2;
   
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   renderCenterPoint();
-  renderTireAndRim(radius, angleOffset);
-  renderVirtualTireBottom(radius, angleOffset);
+  renderTireAndRim(radius * scaleFactor, angleOffset);
+  renderVirtualTireBottom(radius * scaleFactor, angleOffset);
 }
 
 const handleSliderInput = e => {
